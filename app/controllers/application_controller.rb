fe1bc7d8d383a92307c8991b90cd30640be3977d
github username <github email address>
2021-02-16 12:45:39 -0500

    commit tree d0dbbb74c5f59b7cb686ff2e15aeadf318cb86d6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,6 @@ class ApplicationController < Sinatra::Base
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
-
     redirect '/users/home'
   end
 
@@ -37,6 +36,7 @@ class ApplicationController < Sinatra::Base
       redirect '/users/home'
     end
     redirect '/sessions/login'
+    puts params
   end
 
   get '/sessions/logout' do
